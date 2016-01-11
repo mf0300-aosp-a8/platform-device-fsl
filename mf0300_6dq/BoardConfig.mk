c5b3c1cadfa8d027763a9758a0e0b3a28d3b2fb4
p0l1m0rph <andrew.tischenko@gmail.com>
2016-01-11 14:02:04 +0300

    Changing screen resolution

--- mf0300_6dq/BoardConfig.mk
+++ mf0300_6dq/BoardConfig.mk
@@ -27,10 +27,8 @@ USE_GPU_ALLOCATOR := true
 BOARD_KERNEL_CMDLINE := console=ttymxc3,115200 init=/init rootfstype=ext4 video=mxcfb0:dev=ldb,bpp=32 video=mxcfb1:dev=hdmi,if=RGB24,bpp=32 vmalloc=400M androidboot.console=ttymxc3 consoleblank=0 androidboot.hardware=freescale androidboot.selinux=disabled cma=384M 
 # BOARD_KERNEL_CMDLINE := console=ttymxc3,115200 init=/init vmalloc=400M androidboot.console=ttymxc3 consoleblank=0 androidboot.hardware=freescale cma=384M
 
-# TARGET_BOARD_DTS_CONFIG := imx6q:imx6q-sabreauto.dtb imx6dl:imx6dl-sabreauto.dtb imx6qp:imx6qp-sabreauto.dtb
-# TARGET_BOARD_DTS_CONFIG := imx6q-sabreauto.dtb
-TARGET_BOARD_DTS_CONFIG := imx6q-mf0300.dtb
-TARGET_BOOTLOADER_CONFIG := mx6qmf0300_config
+TARGET_BOARD_DTS_CONFIG := imx6q-mf0300-xga.dtb
+TARGET_BOOTLOADER_CONFIG := mx6qmf0300_xga_defconfig
 
 BOARD_SEPOLICY_DIRS := \
        device/fsl/imx6/sepolicy

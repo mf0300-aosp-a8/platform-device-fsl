7d88f69874e19cc3b5ddb554a186b3d889ce09e0
p0l1m0rph <andrew.tischenko@gmail.com>
2016-01-11 14:02:04 +0300

    Add uevent

--- imx6/mf0300_6dq.mk
+++ imx6/mf0300_6dq.mk
@@ -49,4 +49,4 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
 	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
 	device/fsl/mf0300_6dq/required_hardware.xml:system/etc/permissions/required_hardware.xml
-PRODUCT_PACKAGES += AudioRoute
+PRODUCT_PACKAGES += AudioRoute EchoPro

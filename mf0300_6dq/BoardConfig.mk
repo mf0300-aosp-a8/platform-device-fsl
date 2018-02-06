2e8f0bc5536e43b3b88d3d1ef3b7b3e4858a41a2
chakaponden <motometalor@gmail.com>
2018-02-06 17:58:10 +0300

    Set correct u-boot (bootloader) & boot (kernel) build-out image file names

--- mf0300_6dq/BoardConfig.mk
+++ mf0300_6dq/BoardConfig.mk
@@ -41,12 +41,12 @@ USE_GPU_ALLOCATOR := true
 BOARD_KERNEL_CMDLINE := console=ttymxc3,115200 init=/init rootfstype=ext4 video=mxcfb0:dev=ldb,bpp=32 video=mxcfb1:dev=hdmi,if=RGB24,bpp=32 vmalloc=400M androidboot.console=ttymxc3 consoleblank=0 androidboot.hardware=freescale cma=384M 
 
 TARGET_BOARD_DTS_CONFIG := \
-       imx6q-mf0300-xga:imx6q-mf0300-xga.dtb \
-       imx6q-mf0300:imx6q-mf0300.dtb
+	imx6q-mf0300:imx6q-mf0300-wxga.dtb \
+	imx6q-mf0300-xga:imx6q-mf0300-xga.dtb
 
 TARGET_BOOTLOADER_CONFIG := \
-       imx6q-mf0300-xga:mx6qmf0300_xga_defconfig \
-       imx6q-mf0300:mx6qmf0300_defconfig
+	imx6q:mx6qmf0300_wxga_defconfig \
+	imx6q-xga:mx6qmf0300_xga_defconfig
 
 BOARD_SEPOLICY_DIRS := \
        device/fsl/imx6/sepolicy

16174f1d3d27fc892895536fd82c9ab38bde5d65
chakaponden <motometalor@gmail.com>
2018-05-08 13:10:31 +0300

    KERNEL_CMDLINE defined in u-boot; add mfgtools & sdcard flasher u-boot bootloaders

--- mf0300_6dq/BoardConfig.mk
+++ mf0300_6dq/BoardConfig.mk
@@ -38,15 +38,15 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/fsl/imx6
 USE_ION_ALLOCATOR := false
 USE_GPU_ALLOCATOR := true
 
-BOARD_KERNEL_CMDLINE := console=ttymxc3,115200 init=/init rootfstype=ext4 video=mxcfb0:dev=ldb,bpp=32 video=mxcfb1:dev=hdmi,if=RGB24,bpp=32 vmalloc=400M androidboot.console=ttymxc3 consoleblank=0 androidboot.hardware=freescale cma=384M
-
 TARGET_BOARD_DTS_CONFIG := \
-	imx6q-mf0300:imx6q-mf0300-wxga.dtb \
+	imx6q-mf0300-wxga:imx6q-mf0300-wxga.dtb \
 	imx6q-mf0300-xga:imx6q-mf0300-xga.dtb
 
 TARGET_BOOTLOADER_CONFIG := \
-	imx6q-mf0300:mx6qmf0300_wxga_defconfig \
-	imx6q-mf0300-xga:mx6qmf0300_xga_defconfig
+	imx6q-mf0300-wxga:mx6qmf0300_wxga_defconfig \
+	imx6q-mf0300-xga:mx6qmf0300_xga_defconfig \
+	imx6q-mf0300-mfg:mx6qmf0300_mfg_defconfig \
+	imx6q-mf0300-sdcardflasher:mx6qmf0300_sdcardflasher_defconfig
 
 BOARD_SEPOLICY_DIRS := \
        device/fsl/imx6/sepolicy

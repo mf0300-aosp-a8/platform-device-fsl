9267cec66c129950c63ad6403cec59b2cdd70ed2
Den <motometalor@gmail.com>
2018-06-24 18:04:00 +0300

    Devices: fsl: mf0300_6dq: remove additional gpt partition tables
    Override-clean variable ADDITION_BPT_PARTITION.
    Remove gpt additional mf0300_6dq target partition tables.
    Keep only one parition table (variable BOARD_BPT_INPUT_FILES).

--- mf0300_6dq/BoardConfig.mk
+++ mf0300_6dq/BoardConfig.mk
@@ -95,8 +95,7 @@ PRODUCT_COPY_FILES +=	\
 
 # Support gpt
 BOARD_BPT_INPUT_FILES += device/fsl/common/partition/device-partitions-7GB.bpt
-ADDITION_BPT_PARTITION = partition-table-14GB:device/fsl/common/partition/device-partitions-14GB.bpt \
-                         partition-table-28GB:device/fsl/common/partition/device-partitions-28GB.bpt
+ADDITION_BPT_PARTITION =
 
 # Vendor seccomp policy files for media components:
 PRODUCT_COPY_FILES += \

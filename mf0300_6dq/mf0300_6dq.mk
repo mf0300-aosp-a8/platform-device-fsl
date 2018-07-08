5286ef625a73040023e18d3b4d271b9d9080e741
Den <motometalor@gmail.com>
2018-07-08 16:57:56 +0300

    Devices: fsl: mf0300_6dq: remove unused fstab_nand configuration
    Remove unused fstab_nand configuration because mf0300_6dq board does not
    use NAND flash.

--- mf0300_6dq/mf0300_6dq.mk
+++ mf0300_6dq/mf0300_6dq.mk
@@ -5,10 +5,6 @@
 $(call inherit-product, device/fsl/imx6/imx6.mk)
 $(call inherit-product-if-exists,vendor/google/products/gms.mk)
 
-ifneq ($(wildcard device/fsl/mf0300_6dq/fstab_nand.freescale),)
-$(shell touch device/fsl/mf0300_6dq/fstab_nand.freescale)
-endif
-
 ifneq ($(wildcard device/fsl/mf0300_6dq/fstab.freescale),)
 $(shell touch device/fsl/mf0300_6dq/fstab.freescale)
 endif

c678b99b1b235ccdc912f6402d7b0b4a10b21f24
Den <motometalor@gmail.com>
2021-01-16 18:40:57 +0100

    Devices: fsl: mf0300_6dq: update SpeedTest to v4.5.28

--- mf0300_6dq/Speedtest/Android.mk
+++ mf0300_6dq/Speedtest/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := Speedtest
-LOCAL_SRC_FILES := Speedtest_by_Ookla_v4.4.3.apk
+LOCAL_SRC_FILES := Speedtest_by_Ookla_v4.5.28.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 
